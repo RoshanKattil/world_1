0670feb7491ac78c43214e205ab7608d1f81dc54
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-03 02:00:54 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "=========================" << endl;
+    cout << "WELCOME TO WORDLE" << endl;
+    cout << "=========================" << endl;
+    cout << endl;
+
+    cout << "1. Play Wordle" << endl;
+    cout << "2. How to Play" << endl;
+    cout << "3. Statistics Summary" << endl;
+    cout << "4. Reset Statistics" << endl;
+    cout << "5. Exit" << endl;
+    cout << endl;
+
+
+    cout << "Select an option:" << endl;
+    
+    return 0;
+}
